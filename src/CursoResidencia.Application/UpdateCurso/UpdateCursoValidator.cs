ade8371bf53ee0c39f3ea13d8f7c9ba967c44d82
Kevin <kevinjh775@gmail.com>
2022-09-11 16:13:10 -0300

    Validator do endpoint put de curso.

---
+++ src/CursoResidencia.Application/UpdateCurso/UpdateCursoValidator.cs
@@ -0,0 +1,21 @@
+﻿namespace CursoResidencia.Application.UpdateCurso;
+
+public class UpdateCursoValidator : AbstractValidator<UpdateCursoCommand>
+{
+    public UpdateCursoValidator()
+    {
+        RuleFor(x => x.Nome)
+            .NotEmpty();
+
+        RuleFor(x => x.DataInicio)
+            .NotNull();
+
+        RuleFor(x => x.DataFim)
+            .NotNull()
+            .GreaterThanOrEqualTo(x => x.DataInicio);
+
+        RuleFor(x => x.Situacao)
+            .IsInEnum()
+            .NotNull();
+    }
+}
